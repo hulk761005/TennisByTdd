1d03d668b2c8b09c62f7f786093c4bb9af20cedb
hulkchou <hulk.chou@ailabpte.co>
2024-01-15 11:21:43 +0800

    fix: player1 hit the point

--- TennisByTdd/Tennis.cs
+++ TennisByTdd/Tennis.cs
@@ -1,9 +1,19 @@
-﻿namespace TennisByTdd
+﻿
+namespace TennisByTdd
 {
     public class Tennis
     {
+        private int player1Score;
+
+        public void Player1Hit()
+        {
+            player1Score += 1;
+        }
+
         public string Score()
         {
+            if (player1Score == 1) return "fifteen love";
+
             return "love all";
         }
     }
--- TennisByTddTest/TennisTest.cs
+++ TennisByTddTest/TennisTest.cs
@@ -12,5 +12,15 @@ namespace TennisByTddTest
             var score = tennis.Score();
             score.Should().Be("love all");
         }
+
+        [Test]
+        public void Player1_Hit_The_Point() 
+        {
+            var tennis = new Tennis();
+            tennis.Player1Hit();
+            var score = tennis.Score();
+
+            score.Should().Be("fifteen love");
+        }
     }
 }
